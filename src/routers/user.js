101b4bc41251e32d7798e8cf0119572725096acf
Abhishek Sharma <abhi243461@hotmail.com>
2020-06-26 19:57:37 +0530

    refactor: Separater routes

---
+++ src/routers/user.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) return res.status(404).send()
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const validUpdates = ['name', 'email', 'age', 'password']
+    const isValidUpdate = updates.every(update => validUpdates.includes(update))
+
+    if (!isValidUpdate) return res.status(400).send({ error: "Invalid update!" })
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!user) return res.status(404).send()
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if (!user) return res.status(404).send()
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
