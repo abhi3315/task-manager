75ff552d8577cfa07829fef2547f7e2e86a56f85
Abhishek Sharma <abhi243461@hotmail.com>
2020-07-07 16:00:53 +0530

    feat: Cascade delete task

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('../models/task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -89,6 +90,13 @@ userSchema.pre('save', async function (next) {
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
+
+    next()
+})
+
+//delete user tasks
+userSchema.pre('delete', async function (next) {
+    await Task.deleteMany({ owner: this._id })
 
     next()
 })
